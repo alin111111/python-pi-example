482793cdf8c711ce0d72b957c5b7e0ec59db7c90
alin111111 <cs10x_holy@yahoo.com>
2022-02-02 15:14:04 +0200

    main

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # python-pi-example
 This is an example project and i will copystrike you if you copy it bye
+
--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3.8
 
 print ("Hello world")
+
+def main(msg):
+    print(msg)
+
+    main("asd")
